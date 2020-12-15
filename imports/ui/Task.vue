a7614db40d0d0a97aefe9a66e45428671c130d87
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-12-16 00:38:31 +0600

    add vue in meteor

---
+++ imports/ui/Task.vue
@@ -0,0 +1,13 @@
+5  Create Task componentsimple-todos/imports/ui/Task.vue »
+<template>
+  <li>{{ this.task.text }}</li>
+</template>
+ 
+<script>
+export default {
+  props: ["task"],
+  data() {
+    return {};
+  }
+};
+</script>
