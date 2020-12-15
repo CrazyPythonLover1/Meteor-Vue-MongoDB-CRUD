bfd89e10307843f48f31fcae19fd8baead064b77
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-12-16 02:05:18 +0600

    adding a form

--- imports/ui/App.vue
+++ imports/ui/App.vue
@@ -9,6 +9,13 @@
         v-bind:key="task._id"
         v-bind:task="task"
       />
+      <form className="new-task" @submit.prevent="handleSubmit">
+        <input
+          type="text"
+          placeholder="Type to add new tasks"
+          v-model="newTask"
+        />
+      </form>
     </ul>
   </div>
 </template>
@@ -16,16 +23,30 @@
 <script>
 import Vue from "vue";
 import Task from "./Task.vue";
+import AddStudents from "./AddStudents.vue";
 import { Students } from "../api/students.js"
  
 export default {
   components: {
-    Task
+    Task,
+    AddStudents
   },
   data() {
-    return {};
+    return {
+        newTask: ""
+    };
   },
-  methods: {},
+  methods: {
+      handleSubmit(event) {
+      Students.insert({
+        text: this.newTask,
+        createdAt: new Date() // current time
+      });
+ 
+      // Clear form
+      this.newTask = "";
+    }
+  },
   meteor: {
       students() {
           return Students.find({}).fetch();
